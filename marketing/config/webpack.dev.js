6011b0609a3b1e29784535e93586e66414db43d8
Pe5h4 <pesekf@email.cz>
2023-04-21 16:04:59 +0200

    Add react app to marketing

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge'); // used to merge 2 different webpack configs into 1
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin'); // Load module federation plugin
 const commonConfig = require('./webpack.common'); // obtain configuration from webpack.common config
+const packageJson = require('../package.json');
 
 const devConfig = {
 	mode: 'development',
@@ -11,6 +13,23 @@ const devConfig = {
 		}
 	},
 	plugins: [
+		new ModuleFederationPlugin({
+			name: 'marketing', // Global variable
+			filename: 'remoteEntry.js',
+			exposes: { // Exposes the file for sharing
+				'./MarketingApp': './src/bootstrap'
+			},
+			// For React it is recommended to set it to true (we want to have only 1 react version across the applications)
+			// shared: {
+			// 	'react': {
+			// 		singleton: true
+			// 	},
+			// 	'react-dom': {
+			// 		singleton: true
+			// 	}
+			// }
+			shared: packageJson.dependencies, // It will use share of the dependencies from package.json between the applications (and it will diminish sizes of dependency sizes)
+		}),
 		new HtmlWebpackPlugin({
 			template: './public/index.html'
 		})
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 // Mount function to strart up the app
 const mount = (el) => {
-	ReactDOM.render(
-		<h1>Hi there!</h1>,
-		el
-	);
+	ReactDOM.render(<App />, el);
 };
 
 // If we are in the development and in isolation, call mount immediately
