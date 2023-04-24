446e8169622a853ead8ffc86dfc566b3c6e95f22
F.P <45622755+Pe5h4@users.noreply.github.com>
2023-04-24 16:43:55 +0200

    Fixifix

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MaterketingApp from './components/MarketingApp';
 
+// Main app function
 export default () => {
 	return <div><h1> Hi there from container</h1><hr/><MaterketingApp /></div>
 };
