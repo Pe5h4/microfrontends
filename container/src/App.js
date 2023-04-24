d1e76151fafe6feb232342f12bbb4e48ff5266e2
Pe5h4 <pesekf@email.cz>
2023-04-24 18:26:02 +0200

    Remove fixifix

--- container/src/App.js
+++ container/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import MaterketingApp from './components/MarketingApp';
 
-// Main app function
 export default () => {
 	return <div><h1> Hi there from container</h1><hr/><MaterketingApp /></div>
 };
