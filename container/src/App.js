e535a405648096b0207d01b2c343f285846a1f6c
Pe5h4 <pesekf@email.cz>
2023-04-26 16:32:15 +0200

    Add header

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MaterketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
-	return <div><h1> Hi there from container !!!!!!!!</h1><hr/><MaterketingApp /></div>
+	return(
+		<BrowserRouter>
+		<div>
+			<Header />
+			<MaterketingApp />
+		</div>
+		</BrowserRouter>
+	)
 };
