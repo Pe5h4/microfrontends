afcb85b58592d36deffbadb28fc9e24186ea4b91
Pe5h4 <pesekf@email.cz>
2023-04-26 17:23:35 +0200

    Randomize classnames and css styling

--- container/src/App.js
+++ container/src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MaterketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+// It generates the classname for production to avoid css classname collision
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'co'
+})
 
 export default () => {
 	return(
 		<BrowserRouter>
-		<div>
-			<Header />
-			<MaterketingApp />
-		</div>
+			<StylesProvider generateClassName={generateClassName}>
+				<div>
+					<Header />
+					<MaterketingApp />
+				</div>
+			</StylesProvider>
 		</BrowserRouter>
 	)
 };
